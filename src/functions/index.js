5e58e13e91f7f141cef8346ff675330de2440c63
Tomohiko Hiraki <tomohikohiraki@thiraki-osx.local>
2019-09-19 12:01:33 +0900

    デプロイ方法を最新版に移行

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,15 +1,12 @@
-const path = require('path')
-const functions = require('firebase-functions')
-const next = require('next')
+const functions = require('firebase-functions');
+const next = require('next');
 
-var dev = process.env.NODE_ENV !== 'production'
-var app = next({
-  dev,
-  conf: { distDir: `${path.relative(process.cwd(), __dirname)}/next` }
-})
-var handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev, conf: { distDir: 'next' } });
+const handle = app.getRequestHandler();
 
-exports.next = functions.https.onRequest((req, res) => {
-  console.log('File: ' + req.originalUrl) // log the page.js file that is being requested
-  return app.prepare().then(() => handle(req, res))
-})
+exports.next = functions.https.onRequest(async (req, res) => {
+  console.log('File: ' + req.originalUrl); // log the page.js file that is being requested
+  await app.prepare();
+  handle(req, res);
+});
