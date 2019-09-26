caac8681788c5324fcf4d13844dbea42829568d8
thiraki <thiraki@mx.itmedia.co.jp>
2019-09-26 17:37:19 +0900

    ローカル・デプロイ設定完了

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,4 +1,130 @@
 const functions = require('firebase-functions');
-const next = require('./server');
+const next = require('next')
+const dev = process.env.NODE_ENV !== 'production'
+let app
+if (dev) {
+  app = next({
+    dev,
+    // conf: { distDir: 'next' },
+    dir: '../app'
+  });
+} else {
+  app = next({
+    dev,
+    conf: { distDir: 'next' },
+    // dir: '../app'
+  });
+}
 
-exports.next = functions.https.onRequest(next);
+const handle = app.getRequestHandler()
+const express = require('express')
+const server = express()
+
+app.prepare()
+  .then(() => {
+
+    server.get('/events/:id/reception', (req, res) => {
+      const actualPage = '/events/reception'
+      const queryParams = { id: req.params.id }
+      app.render(req, res, actualPage, queryParams)
+    })
+
+    server.get('/events/:id/reception/show', (req, res) => {
+      const actualPage = '/events/reception/show'
+      const queryParams = { id: req.params.id }
+      app.render(req, res, actualPage, queryParams)
+    })
+
+    server.get('/events/:id/reception/read', (req, res) => {
+      const actualPage = '/events/reception/read'
+      const queryParams = { id: req.params.id }
+      app.render(req, res, actualPage, queryParams)
+    })
+
+    server.get('/events/reception/:id/edit', (req, res) => {
+      const actualPage = '/events/reception/edit'
+      const queryParams = { id: req.params.id }
+      app.render(req, res, actualPage, queryParams)
+    })
+
+    server.get('/events/:id', (req, res) => {
+      const actualPage = '/events/show'
+      const queryParams = { id: req.params.id }
+      app.render(req, res, actualPage, queryParams)
+    })
+
+    server.get('/events/:id/edit', (req, res) => {
+      const actualPage = '/events/edit'
+      const queryParams = { id: req.params.id }
+      app.render(req, res, actualPage, queryParams)
+    })
+
+    server.get('/events/:id/confirm', (req, res) => {
+      const actualPage = '/events/confirm'
+      const queryParams = { id: req.params.id }
+      app.render(req, res, actualPage, queryParams)
+    })
+
+    server.get('/events/:id/purchase', (req, res) => {
+      const actualPage = '/events/purchase'
+      const queryParams = { id: req.params.id, familyName: req.query.familyName, firstName: req.query.firstName, email: req.query.email }
+      app.render(req, res, actualPage, queryParams)
+    })
+
+    server.get('/events/:id/purchase/confirm', (req, res) => {
+      const actualPage = '/events/purchase/confirm'
+      const queryParams = { id: req.params.id, familyName: req.query.familyName, firstName: req.query.firstName, email: req.query.email }
+      app.render(req, res, actualPage, queryParams)
+    })
+
+    server.get('/events/:id/purchase/checkout', (req, res) => {
+      const actualPage = '/events/purchase/checkout'
+      const queryParams = { id: req.params.id }
+      app.render(req, res, actualPage, queryParams)
+    })
+
+    server.get('/events/:id/report', (req, res) => {
+      const actualPage = '/events/report'
+      const queryParams = { id: req.params.id }
+      app.render(req, res, actualPage, queryParams)
+    })
+
+    server.get('/users/:id', (req, res) => {
+      const actualPage = '/users/show'
+      const queryParams = { id: req.params.id }
+      app.render(req, res, actualPage, queryParams)
+    })
+
+    server.get('/users/:id/edit', (req, res) => {
+      const actualPage = '/users/edit'
+      const queryParams = { id: req.params.id }
+      app.render(req, res, actualPage, queryParams)
+    })
+
+    server.get('/users/:id/myEvents', (req, res) => {
+      const actualPage = '/users/show/myEvents'
+      const queryParams = {
+        id: req.params.id
+      }
+      app.render(req, res, actualPage, queryParams)
+    })
+
+    server.get('/users/:id/myTickets', (req, res) => {
+      const actualPage = '/users/show/myTickets'
+      const queryParams = {
+        id: req.params.id
+      }
+      app.render(req, res, actualPage, queryParams)
+    })
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(3000, err => {
+      if (err) throw err
+      console.log('> Ready on http://localhost:3000')
+    })
+  })
+
+exports.nextApp = functions.https.onRequest(server)
