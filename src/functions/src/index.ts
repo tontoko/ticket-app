205337379d0f6702774891689a4c0656c4cb2890
Tomohiko Hiraki <hirakitomohiko@gmail.com>
2020-03-30 15:11:57 +0900

    ci修正

--- src/functions/src/index.ts
+++ src/functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from 'firebase-functions';
 const admin = require('firebase-admin')
-admin.initializeApp(functions.config().firebase)
+admin.initializeApp()
 const fireStore = admin.firestore()
 
 exports.createUser = functions
