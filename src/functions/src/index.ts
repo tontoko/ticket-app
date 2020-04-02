1efd73d63c738ca1d13a9be4579e1d7afbbac931
hiraki-tomohiko <hiraki-tomohiko@PC-0001415.local>
2020-04-02 19:04:22 +0900

    functionsでstripeの顧客データ作成するように変更

--- src/functions/src/index.ts
+++ src/functions/src/index.ts
@@ -1,5 +1,16 @@
-import * as functions from 'firebase-functions';
-const admin = require('firebase-admin')
+
+type stripeEnv = {
+  endpoint: {
+    prod: string,
+    dev: string
+  }
+}
+const stripeEnv = functions.config().stripe as stripeEnv
+const stripeSecret = process.env.GCLOUD_PROJECT === 'ticket-app-d3f5a' ? stripeEnv.endpoint.prod : stripeEnv.endpoint.dev
+const stripe = require('stripe')(stripeSecret)
+
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin' 
 admin.initializeApp(functions.config().firebase)
 const fireStore = admin.firestore()
 
@@ -9,5 +20,10 @@ exports.createUser = functions
   const usersRef = fireStore.collection('users');
   usersRef.doc(user.uid).set({
     admin: false
+  })
+  const account = stripe.accounts.create({
+    country: 'JP',
+    type: 'custom',
+    requested_capabilities: ['card_payments'],
   })
 });
