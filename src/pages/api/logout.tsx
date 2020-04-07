fc499de0e011f5b29d4ac41ff8652ccaf5398a7b
Tomohiko <hiraki-tomohiko@dmm.com>
2020-04-07 18:59:29 +0900

    環境変数テスト

--- src/pages/api/logout.tsx
+++ src/pages/api/logout.tsx
@@ -1,8 +1,15 @@
 import getSession from '@/src/lib/session'
 import { NextApiHandler } from 'next'
+import fs from 'fs'
 
 const endpoint: NextApiHandler = (async (req, res) => {
-    // micro_session(req, res)
+    // テスト
+    console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+    const file = fs.readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+    console.log(file)
+
+
+
     let session = await(await getSession())(req, res)
     session.token = null
     res.json({ status: true })
