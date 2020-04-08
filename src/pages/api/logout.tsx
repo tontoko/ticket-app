b024843b32cd250391de55c0f15d7fd149f877e3
Tomohiko <hiraki-tomohiko@dmm.com>
2020-04-08 14:20:40 +0900

    テスト用コード削除

--- src/pages/api/logout.tsx
+++ src/pages/api/logout.tsx
@@ -3,13 +3,6 @@ import { NextApiHandler } from 'next'
 import fs from 'fs'
 
 const endpoint: NextApiHandler = (async (req, res) => {
-    // テスト
-    console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS)
-    const file = fs.readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS)
-    console.log(file.toString())
-
-
-
     let session = await(await getSession())(req, res)
     session.token = null
     res.json({ status: true })
