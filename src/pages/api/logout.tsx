5af42ce29e975773ee26fd5d89482bcdea8c1640
Tomohiko <hiraki-tomohiko@dmm.com>
2020-04-08 14:52:19 +0900

    テスト

--- src/pages/api/logout.tsx
+++ src/pages/api/logout.tsx
@@ -1,8 +1,10 @@
 import getSession from '@/src/lib/session'
 import { NextApiHandler } from 'next'
-import fs from 'fs'
 
 const endpoint: NextApiHandler = (async (req, res) => {
+    // テスト
+    console.log(process.env)
+
     let session = await(await getSession())(req, res)
     session.token = null
     res.json({ status: true })
