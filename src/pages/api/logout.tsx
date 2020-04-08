5ca9fc76d0e7c8507f3316343f54bb5b1abf1ca6
Tomohiko <hiraki-tomohiko@dmm.com>
2020-04-08 18:57:04 +0900

    デバック用コード削除

--- src/pages/api/logout.tsx
+++ src/pages/api/logout.tsx
@@ -1,10 +1,7 @@
 import getSession from '@/src/lib/session'
 import { NextApiHandler } from 'next'
 
-const endpoint: NextApiHandler = (async (req, res) => {
-    // テスト
-    console.log(process.env)
-
+const endpoint: NextApiHandler = (async (req, res) => {�
     let session = await(await getSession())(req, res)
     session.token = null
     res.json({ status: true })
