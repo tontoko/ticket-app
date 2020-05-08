b4b4e43a2b7b8a4ca839fed54c392914930d55af
Tomohiko <hiraki-tomohiko@dmm.com>
2020-05-08 19:23:11 +0900

    cloudfunctionsにpaymentを載せ替え

--- src/functions/src/index.ts
+++ src/functions/src/index.ts
@@ -3,9 +3,11 @@ import * as admin from 'firebase-admin'
 import Stripe from 'stripe'
 
 type stripeEnv = {
-  endpoint: {
-    prod: string,
-    dev: string
+  payment: {
+    endpoint: {
+      prod: string,
+      dev: string
+    },
   },
   apikey: {
     prod: string,
@@ -14,10 +16,11 @@ type stripeEnv = {
 }
 
 admin.initializeApp(functions.config().firebase)
-const fireStore = admin.firestore()
+const firestore = admin.firestore()
 
 const stripeEnv = functions.config().stripe as stripeEnv
 const stripeSecret = process.env.GCLOUD_PROJECT === 'ticket-app-d3f5a' ? stripeEnv.apikey.prod : stripeEnv.apikey.dev
+const stripePaymentSecret = process.env.GCLOUD_PROJECT === 'ticket-app-d3f5a' ? stripeEnv.payment.endpoint.prod : stripeEnv.payment.endpoint.dev
 // @ts-ignore
 const stripe = new Stripe(stripeSecret, { apiVersion: null })
 
@@ -30,9 +33,72 @@ exports.createUser = functions
     requested_capabilities: ['card_payments', 'transfers'],
     email: user.email
   })
-  const usersRef = fireStore.collection('users');
+  const usersRef = firestore.collection('users');
   await usersRef.doc(user.uid).set({
     admin: false,
     stripeId: stripeAccount.id
   })
 });
+
+exports.payment = functions
+.region('asia-northeast1')
+.https.onRequest(async (req, res) => {
+  const sig = req.headers['stripe-signature'];
+  const { body } = req
+  let webhockEvent: Stripe.Event
+
+  try {
+    const endpointSecret = stripePaymentSecret
+    webhockEvent = stripe.webhooks.constructEvent(body, sig, endpointSecret);
+  } catch (err) {
+    // invalid signature
+    console.error(err)
+    res.status(400).end();
+    return;
+  }
+
+  let intent: Stripe.PaymentIntent
+  try {
+    switch (webhockEvent['type']) {
+      case 'payment_intent.succeeded':
+        intent = webhockEvent.data.object as Stripe.PaymentIntent
+        const { event, category } = intent.metadata
+        const firestore = admin.firestore()
+        try {
+          await firestore.runTransaction(async transaction => {
+            const categoryRef = firestore.collection('events').doc(event).collection('categories').doc(category)
+            const categoryResult = (await transaction.get(categoryRef)).data()
+            const stock = categoryResult && categoryResult.stock
+            if (!stock || stock === 0) {
+              // 在庫なしの返金処理
+              const refunds = await stripe.refunds.create({
+                payment_intent: intent.id,
+                refund_application_fee: true,
+                reverse_transfer: true
+              })
+              // Intentをキャンセル
+              await stripe.paymentIntents.cancel(intent.id);
+              throw Error('在庫がありませんでした。')
+            }
+            // 在庫を一つ減らす
+            transaction.set(categoryRef, {
+              stock: stock - 1
+            }, { merge: true })
+          })
+        } catch (e) {
+          console.log(`${e}:`, intent.id);
+        }
+        console.log("Succeeded:", intent.id);
+        break;
+      case 'payment_intent.payment_failed':
+        intent = webhockEvent.data.object as Stripe.PaymentIntent
+        const message = intent.last_payment_error && intent.last_payment_error.message;
+        console.error('Failed:', intent.id, message);
+        break;
+    }
+  } catch (e) {
+    console.error(e)
+  }
+
+  res.status(200).end()
+})
