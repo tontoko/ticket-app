bc466e3a4d767140ff6db6b3222cf80885b07017
hiraki-tomohiko <hiraki-tomohiko@PC-0001452.local>
2020-07-15 00:00:57 +0900

    next-pwa対応

--- next.config.js
+++ next.config.js
@@ -1,19 +1,21 @@
-const path = require('path')
+const withPWA = require("next-pwa");
 const withMDX = require('@next/mdx')()
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 
-module.exports = withMDX({
-  target: "serverless",
-  cssModule: true,
-  webpack: (config) => {
-    config.node = {
-      fs: 'empty'
-    }
-    config.plugins.push(
-      new MomentLocalesPlugin({
-        localesToKeep: ["ja"],
-      })
-    );
-    return config;
-  },
-});
+module.exports = withPWA(
+  withMDX({
+    target: "serverless",
+    cssModule: true,
+    pwa: {
+      dest: "public",
+    },
+    webpack: (config) => {
+      config.plugins.push(
+        new MomentLocalesPlugin({
+          localesToKeep: ["ja"],
+        })
+      );
+      return config;
+    },
+  })
+);
