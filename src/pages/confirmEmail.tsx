176b193c9c0ad83e678ba9b65112e283a6d4799c
hiraki-tomohiko <hiraki-tomohiko@PC-0001452.local>
2020-07-31 16:35:39 +0900

    確認メール送信部分を修正

--- src/pages/confirmEmail.tsx
+++ src/pages/confirmEmail.tsx
@@ -5,40 +5,37 @@ import errorMsg from '@/src/lib/errorMsg'
 import Loading from '@/src/components/loading'
 import { auth } from '../lib/initFirebase'
 
-const ConfirmEmail = ({user}) => {
-    const router = useRouter()
-    const [msg, setMsg] = useState('')
-    const [loading, setLoading] = useState(true)
-    
-    useEffect(() => {
-        (async() => {
-            // クライアント側の認証を待つuser
-            if (!user) return;
-            if (!loading) return
-            sendEmail();
-        })()
-    })
+const ConfirmEmail = ({ user, userLoading }) => {
+  const router = useRouter();
+  const [msg, setMsg] = useState("");
+  const [loading, setLoading] = useState(true);
 
-    const sendEmail = async () => {
-        try {
-            await user.sendEmailVerification();
-            setLoading(false)
-            setMsg('登録されたメールアドレスに認証用メールを送信しました。')
-        } catch (e) {
-            console.log(e)
-            setLoading(false)
-            setMsg(errorMsg(e))
-        }
-        setTimeout(async() => {
-            await auth.signOut()
-            router.push('/login')
-        }, 5000)
+  useEffect(() => {
+    (async () => {
+      // クライアント側の認証を待つuser
+      if (!user || userLoading || !loading) return;
+      sendEmail();
+    })();
+  }, [user, userLoading]);
+
+  const sendEmail = async () => {
+    try {
+      await user.sendEmailVerification();
+      setLoading(false);
+      setMsg("登録されたメールアドレスに認証用メールを送信しました。");
+    } catch (e) {
+      console.log(e);
+      setLoading(false);
+      setMsg(errorMsg(e));
     }
+    setTimeout(async () => {
+      await auth.signOut();
+      router.push("/login");
+    }, 5000);
+  };
 
-    if (loading) return <Loading/>
-    return (
-    <h4>{msg}</h4>
-    )
-}
+  if (loading) return <Loading />;
+  return <h4>{msg}</h4>;
+};
 
 export default ConfirmEmail
