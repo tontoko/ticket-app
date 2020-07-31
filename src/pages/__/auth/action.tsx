2c764465915937172b86c592778ed3803dbfb5be
hiraki-tomohiko <hiraki-tomohiko@PC-0001452.local>
2020-07-31 17:04:09 +0900

    メールアクションが2回実行されるのを修正

--- src/pages/__/auth/action.tsx
+++ src/pages/__/auth/action.tsx
@@ -8,7 +8,7 @@ import {useRouter} from 'next/router'
 import ResetPassword from './resetPassword'
 import { encodeQuery } from '@/src/lib/parseQuery'
 
-const Action = ({user}) => {
+const Action = ({user, userLoading}) => {
     const alert = useAlert()
     const router = useRouter()
     const [loading, setLoading] = useState(true)
@@ -18,37 +18,36 @@ const Action = ({user}) => {
     let oobCode = ''
 
     useEffect(() => {
-        (async() => {
-            if (!loading) return
-            if (!router) return
-            mode = router.query.mode as string;
-            oobCode = router.query.oobCode as string;
-            try {
-                switch (mode) {
-                    case 'resetPassword':
-                        // Display reset password handler and UI.
-                        handleResetPassword()
-                        return
-                    case 'recoverEmail':
-                        // Display email recovery handler and UI.
-                        await handleRecoverEmail()
-                        setTimeout(() => redirectAfterUpdate(), 5000)
-                        return
-                    case 'verifyEmail':
-                        // Display email verification handler and UI.
-                        await handleVerifyEmail()
-                        setTimeout(() => redirectAfterUpdate(), 5000)
-                        return
-                    default:
-                        // Error: invalid mode.
-                        throw new Error("不正なリクエストです。")
-                }
-            } catch (e) {
-                alert.error(errorMsg(e))
-                setTimeout(() => redirectAfterUpdate(), 5000)
-            }
-        })()
-    }, [router])
+      (async () => {
+        if (!loading || userLoading || !router) return;
+        mode = router.query.mode as string;
+        oobCode = router.query.oobCode as string;
+        try {
+          switch (mode) {
+            case "resetPassword":
+              // Display reset password handler and UI.
+              handleResetPassword();
+              return;
+            case "recoverEmail":
+              // Display email recovery handler and UI.
+              await handleRecoverEmail();
+              setTimeout(() => redirectAfterUpdate(), 5000);
+              return;
+            case "verifyEmail":
+              // Display email verification handler and UI.
+              await handleVerifyEmail();
+              setTimeout(() => redirectAfterUpdate(), 5000);
+              return;
+            default:
+              // Error: invalid mode.
+              throw new Error("不正なリクエストです。");
+          }
+        } catch (e) {
+          alert.error(errorMsg(e));
+          setTimeout(() => redirectAfterUpdate(), 5000);
+        }
+      })();
+    }, [router, userLoading]);
 
     const redirectAfterUpdate = async(msg?:string) => {
         if (user) await auth.signOut()
