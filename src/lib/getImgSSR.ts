b01e9f3e4a0887903fc934edad9dfe850553a79b
Tomohiko Hiraki <hirakitomohiko@gmail.com>
2020-08-04 22:29:18 +0900

    画像のサインURLの期間変更とアクセスルール修正

--- src/lib/getImgSSR.ts
+++ src/lib/getImgSSR.ts
@@ -11,23 +11,23 @@ export default async (img:string, uid:string, size?: '360'|'800') => {
   const anotherSuffix = size ? '_360x360.jpg' : `_800x800.jpg`
 
   let result:boolean
-  if (!img) return (await storage.file(`${defaultPath}${sizeSuffix}`).getSignedUrl({ action: 'read', expires: date.setHours(date.getHours()+1) }))[0] as string
+  if (!img) return (await storage.file(`${defaultPath}${sizeSuffix}`).getSignedUrl({ action: 'read', expires: date.setFullYear(date.getFullYear() + 1) }))[0] as string
 
   [result] = await storage.file(`${filepath}${sizeSuffix}`).exists()
   if (result) {
-    return (await storage.file(`${filepath}${sizeSuffix}`).getSignedUrl({ action: 'read', expires: date.setHours(date.getHours() + 1) }))[0] as string
+    return (await storage.file(`${filepath}${sizeSuffix}`).getSignedUrl({ action: 'read', expires: date.setFullYear(date.getFullYear() + 1) }))[0] as string
   }
 
   [result] = await storage.file(`${filepath}${anotherSuffix}`).exists()
   if (result) {
-    return (await storage.file(`${filepath}${anotherSuffix}`).getSignedUrl({ action: 'read', expires: date.setHours(date.getHours() + 1) }))[0] as string
+    return (await storage.file(`${filepath}${anotherSuffix}`).getSignedUrl({ action: 'read', expires: date.setFullYear(date.getFullYear() + 1) }))[0] as string
   }
   
   [result] = await storage.file(`${filepath}.jpg`).exists()
   if (result) {
-    return (await storage.file(`${filepath}.jpg`).getSignedUrl({ action: 'read', expires: date.setHours(date.getHours() + 1) }))[0] as string
+    return (await storage.file(`${filepath}.jpg`).getSignedUrl({ action: 'read', expires: date.setFullYear(date.getFullYear() + 1) }))[0] as string
   }
 
-  return (await storage.file(`${defaultPath}${sizeSuffix}`).getSignedUrl({ action: 'read', expires: date.setHours(date.getHours() + 1) }))[0] as string
+  return (await storage.file(`${defaultPath}${sizeSuffix}`).getSignedUrl({ action: 'read', expires: date.setFullYear(date.getFullYear() + 1) }))[0] as string
 
 }
