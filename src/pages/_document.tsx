877b642b371c30b78b391778ba4a3d95f7db4881
hiraki-tomohiko <hiraki-tomohiko@PC-0001452.local>
2020-08-14 00:41:09 +0900

    広告追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,8 +8,14 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang='ja' title='Ticket-App'>
-        <Head />
+      <Html lang="ja" title="Ticket-App">
+        <Head>
+          <script
+            data-ad-client="ca-pub-6198405782288989"
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          ></script>
+        </Head>
         <body>
           <Main />
           <NextScript />
