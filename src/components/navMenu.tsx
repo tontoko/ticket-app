910a729d50f6d7fd7497c15e4c1c9377c1aa2646
hiraki-tomohiko <hiraki-tomohiko@PC-0001452.local>
2020-08-13 12:11:43 +0900

    ナビバーの表示修正

---
+++ src/components/navMenu.tsx
@@ -0,0 +1,121 @@
+import Link from "next/link"
+import notifies from "../pages/users/[id]/notifies"
+import { NavbarToggler, Collapse, Nav, NavItem, NavLink } from "reactstrap"
+
+export const LoginMenu = ({user, tmpUser, avater, toggle, isOpen}) => {
+
+    return (
+          <>
+            <Link href={`/users/${user ? user.uid : tmpUser.uid}/edit`}>
+              <div className="ml-auto">
+                <img
+                  src={avater}
+                  style={{
+                    borderRadius: "50%",
+                    height: "40px",
+                    width: "40px",
+                    cursor: "pointer",
+                  }}
+                />
+              </div>
+            </Link>
+            <Link href={`/users/${user ? user.uid : tmpUser.uid}/notifies`}>
+              <div
+                className="ml-2 mr-2"
+                style={{
+                  border: "solid 1px gray",
+                  borderRadius: "50%",
+                  backgroundColor: notifies?.length > 0 ? "orange" : "gray",
+                  width: "40px",
+                  height: "40px",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  cursor: "pointer",
+                }}
+              >
+                <div
+                  style={{
+                    borderRadius: "50%",
+                    backgroundColor: "white",
+                    width: "32px",
+                    height: "32px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <div style={{ fontWeight: "bold" }}>{notifies?.length}</div>
+                </div>
+              </div>
+            </Link>
+            <NavbarToggler onClick={() => toggle(!isOpen)} />
+            <Collapse
+              isOpen={isOpen}
+              navbar
+              className="justify-content-end flex-grow-0"
+            >
+              <Nav navbar>
+                <NavItem style={{ cursor: "pointer" }}>
+                  <Link
+                    href={`/users/${user ? user.uid : tmpUser.uid}/myEvents`}
+                  >
+                    <NavLink style={{ color: "white" }}>
+                      主催するイベント
+                    </NavLink>
+                  </Link>
+                </NavItem>
+                <NavItem style={{ cursor: "pointer" }}>
+                  <Link
+                    href={`/users/${user ? user.uid : tmpUser.uid}/myTickets`}
+                  >
+                    <NavLink style={{ color: "white" }}>
+                      購入済みチケット
+                    </NavLink>
+                  </Link>
+                </NavItem>
+                <NavItem style={{ cursor: "pointer" }}>
+                  <Link href={`/termsOfUse`}>
+                    <NavLink style={{ color: "white" }}>利用規約</NavLink>
+                  </Link>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </>
+        
+    )
+}
+
+    
+
+export const LogoutMenu = ({ toggle, isOpen }) => {
+  return (
+    <>
+      <div style={{ marginLeft: "auto" }} />
+      <NavbarToggler onClick={() => toggle(!isOpen)} />
+      <Collapse
+        isOpen={isOpen}
+        navbar
+        className="justify-content-end flex-grow-0"
+      >
+        <Nav navbar>
+          <NavItem style={{ cursor: "pointer" }}>
+            <Link href={`/register`}>
+              <NavLink>新規登録する</NavLink>
+            </Link>
+          </NavItem>
+          <NavItem style={{ cursor: "pointer" }}>
+            <Link href={`/login`}>
+              <NavLink>ログイン</NavLink>
+            </Link>
+          </NavItem>
+          <NavItem style={{ cursor: "pointer" }}>
+            <Link href={`/termsOfUse`}>
+              <NavLink>利用規約</NavLink>
+            </Link>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </>
+  );
+};
--- src/pages/layouts/userLayouts.tsx
+++ src/pages/layouts/userLayouts.tsx
@@ -1,45 +1,48 @@
 import Link from 'next/link'
-import React, { lazy, Suspense } from 'react'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo, Suspense } from 'react'
 import {
     Navbar, NavbarBrand, NavbarToggler, Collapse, NavLink, Nav, NavItem, Container } from 'reactstrap'
-const Avater = lazy(() => import('react-avatar'))
 import { useAlert } from "react-alert"
 import { useRouter } from 'next/router'
 import { decodeQuery } from '@/src/lib/parseQuery'
-import { fuego, useCollection } from "@nandorojo/swr-firestore";
+import { useCollection } from "@nandorojo/swr-firestore";
+import { LoginMenu, LogoutMenu } from "@/src/components/navMenu";
 
 const UserLayout: React.FC<any> = ({ user, tmpUser, children }) => {
   const router = useRouter()
   const alert = useAlert()
   const [isOpen, toggle] = useState(false)
-  const [facebookUid, setFacebookUid] = useState();
-  const [googleUid, setGoogleUid] = useState();
+  const [Menu, setMenu] = useState(<></>)
   const { data: notifies } = useCollection(user && `users/${user.uid}/notifies`, {
     where: ["read", "==", false],
     listen: true
   });
-  
-  useEffect(() => {
-    if ((user || tmpUser) && !facebookUid && !googleUid) {
+
+  const avater = useMemo(() => {
+    if (user || tmpUser) {
       const providerData = user
         ? user.providerData[0]
         : tmpUser.providerData[0];
-      if (providerData.providerId === "facebook.com") {
-        setFacebookUid(providerData.uid);
-      }
-      if (providerData.providerId === "google.com") {
-        setGoogleUid(providerData.uid);
-      }
+      if (providerData.photoURL) return providerData.photoURL;
     }
-    if (!user) return
-  }, [user]);
+    return "/icons/person-icon-default.png"
+  }, [user, tmpUser])
+
+  useEffect(() => {
+    setMenu(
+      (user || tmpUser) ? (
+        <LoginMenu {...{ user, tmpUser, avater, toggle, isOpen }} />
+      ) : (
+        <LogoutMenu {...{ avater, toggle, isOpen }} />
+      )
+    );
+  }, [avater])
 
   useEffect(() => {
     if (!router) return
     const { msg } = router.query;
     if (msg) alert.success(decodeQuery(msg as string));
-  }, [!!router, router.query.msg])
+  }, [router, router.query.msg])
 
   return (
     <>
@@ -49,118 +52,7 @@ const UserLayout: React.FC<any> = ({ user, tmpUser, children }) => {
             <NavbarBrand style={{ cursor: "pointer" }}>Ticket-App</NavbarBrand>
           </div>
         </Link>
-        {(user || tmpUser) && (
-          <>
-            <Suspense fallback={<div className="ml-auto mr-2" />}>
-              <Link href={`/users/${user ? user.uid : tmpUser.uid}/edit`}>
-                <div className="ml-auto mr-2">
-                  <Avater
-                    size="40"
-                    round
-                    style={{
-                      cursor: "pointer",
-                      backgroundColor: "lightgray",
-                    }}
-                    facebookId={facebookUid}
-                    googleId={googleUid}
-                    src="/icons/person-icon-default.png"
-                  />
-                </div>
-              </Link>
-              <Link href={`/users/${user ? user.uid : tmpUser.uid}/notifies`}>
-                <div
-                  className="mr-2"
-                  style={{
-                    border: "solid 1px gray",
-                    borderRadius: "50%",
-                    backgroundColor: notifies?.length > 0 ? "orange" : "gray",
-                    width: "40px",
-                    height: "40px",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    cursor: "pointer",
-                  }}
-                >
-                  <div
-                    style={{
-                      borderRadius: "50%",
-                      backgroundColor: "white",
-                      width: "32px",
-                      height: "32px",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }}
-                  >
-                    <div style={{ fontWeight: "bold" }}>{notifies?.length}</div>
-                  </div>
-                </div>
-              </Link>
-            </Suspense>
-            <NavbarToggler onClick={() => toggle(!isOpen)} />
-            <Collapse
-              isOpen={isOpen}
-              navbar
-              className="justify-content-end flex-grow-0"
-            >
-              <Nav navbar>
-                <NavItem style={{ cursor: "pointer" }}>
-                  <Link
-                    href={`/users/${user ? user.uid : tmpUser.uid}/myEvents`}
-                  >
-                    <NavLink style={{ color: "white" }}>
-                      主催するイベント
-                    </NavLink>
-                  </Link>
-                </NavItem>
-                <NavItem style={{ cursor: "pointer" }}>
-                  <Link
-                    href={`/users/${user ? user.uid : tmpUser.uid}/myTickets`}
-                  >
-                    <NavLink style={{ color: "white" }}>
-                      購入済みチケット
-                    </NavLink>
-                  </Link>
-                </NavItem>
-                <NavItem style={{ cursor: "pointer" }}>
-                  <Link href={`/termsOfUse`}>
-                    <NavLink style={{ color: "white" }}>利用規約</NavLink>
-                  </Link>
-                </NavItem>
-              </Nav>
-            </Collapse>
-          </>
-        )}
-        {!user && !tmpUser && (
-          <>
-            <div style={{ marginLeft: "auto" }} />
-            <NavbarToggler onClick={() => toggle(!isOpen)} />
-            <Collapse
-              isOpen={isOpen}
-              navbar
-              className="justify-content-end flex-grow-0"
-            >
-              <Nav navbar>
-                <NavItem style={{ cursor: "pointer" }}>
-                  <Link href={`/register`}>
-                    <NavLink>新規登録する</NavLink>
-                  </Link>
-                </NavItem>
-                <NavItem style={{ cursor: "pointer" }}>
-                  <Link href={`/login`}>
-                    <NavLink>ログイン</NavLink>
-                  </Link>
-                </NavItem>
-                <NavItem style={{ cursor: "pointer" }}>
-                  <Link href={`/termsOfUse`}>
-                    <NavLink>利用規約</NavLink>
-                  </Link>
-                </NavItem>
-              </Nav>
-            </Collapse>
-          </>
-        )}
+        {Menu}
       </Navbar>
       <Container style={{ marginTop: "2em", marginBottom: "2em" }}>
         {children}
