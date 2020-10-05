066ab746976c745342b079ee0cf9913d1e56c8b8
Tomohiko Hiraki <hirakitomohiko@gmail.com>
2020-10-05 22:41:55 +0900

    テスト用のadmin sdk設定修正

--- src/lib/initFirebaseAdmin.ts
+++ src/lib/initFirebaseAdmin.ts
@@ -4,10 +4,8 @@ import admin from "firebase-admin";
 const initFirebaseAdmin = async () => {
   const firebase = await import("firebase-admin");
   const params = process.env.ENV === "prod" ? prod : dev;
-  let init = true
 
   if (!firebase.apps.length) {
-    init = false
     if (process.env.NODE_ENV === "test") {
       firebase.initializeApp();
     } else {
@@ -27,11 +25,6 @@ const initFirebaseAdmin = async () => {
   const firestore = firebase.firestore();
   const storage = firebase.storage().bucket(params.storageBucket);
   const messaging = firebase.messaging();
-  if (!init && process.env.NODE_ENV === "test")
-    firestore.settings({
-      host: "localhost:8080",
-      ssl: false,
-    });
 
   return { firebase, firestore, storage, messaging };
 }
--- test/setupTests.ts
+++ test/setupTests.ts
@@ -1,3 +1,4 @@
 import { configure } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 configure({ adapter: new EnzymeAdapter() });
+process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080'
