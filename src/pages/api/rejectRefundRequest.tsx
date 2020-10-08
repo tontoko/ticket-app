7f9f65d2325f4162e876683aab10a3271d20e6b4
Hiraki Tomohiko <hirakitomohiko@gmail.com>
2020-10-08 15:12:19 +0900

    返金周りの不要パラメータ削除

--- src/pages/api/rejectRefundRequest.tsx
+++ src/pages/api/rejectRefundRequest.tsx
@@ -1,45 +1,43 @@
-import initFirebaseAdmin from "@/src/lib/initFirebaseAdmin";
-import { NextApiHandler } from "next";
+import initFirebaseAdmin from '@/src/lib/initFirebaseAdmin'
+import { NextApiHandler } from 'next'
 
 const sendRefundRequest: NextApiHandler = async (req, res) => {
   const { firestore, firebase } = await initFirebaseAdmin()
-  const {
-    reason,
-    reasonText,
-    detailText,
-    seller,
-    buyer,
-    paymentId,
-    token,
-  } = req.body;
+  const { reason, seller, buyer, paymentId, token } = req.body
 
-  const decodedIdToken =  await firebase.auth().verifyIdToken(token)
+  const decodedIdToken = await firebase.auth().verifyIdToken(token)
 
   if (decodedIdToken.uid !== seller) return res.status(500).end()
 
-  await firestore.collection("payments").doc(paymentId).set({
-    refund: {
-      rejected: true
-    },
-  }, { merge: true });
-  await firestore.collection("contacts").add({
-    category: "refund",
-    text: detailText,
+  await firestore
+    .collection('payments')
+    .doc(paymentId)
+    .set(
+      {
+        refund: {
+          rejected: true,
+        },
+      },
+      { merge: true },
+    )
+  await firestore.collection('contacts').add({
+    category: 'refund',
+    text: '返金申請が却下されました。調査を行ってください。',
     info: {
       paymentId,
       reason,
     },
-  });
+  })
   await firestore
-    .collection("users")
+    .collection('users')
     .doc(buyer)
-    .collection("notifies")
+    .collection('notifies')
     .add({
-      text: "返金申請が却下されました。サービス管理者の審査をお待ちください。",
+      text: '返金申請が却下されました。サービス管理者の審査をお待ちください。',
       url: `/users/${buyer}/payments/${paymentId}`,
       read: false,
       createdAt: new Date(),
-    });
+    })
   res.status(200).end()
 }
 
