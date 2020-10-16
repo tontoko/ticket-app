c622f08fdc7504d11c183f0073afb55e2cb8ec66
Hiraki Tomohiko <hirakitomohiko@gmail.com>
2020-10-17 01:05:02 +0900

    主催イベントページを静的ページ化

--- src/pages/users/[id]/myEvents.tsx
+++ src/pages/users/[id]/myEvents.tsx
@@ -1,16 +1,15 @@
-import React, { useState, useEffect, useMemo } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Card, CardText, CardBody, CardTitle, CardSubtitle, Button, Col, Row } from 'reactstrap'
 import Link from 'next/link'
-import getImg from '@/src/lib/getImgSSR'
+import getImg from '@/src/lib/getImg'
 import moment from 'moment'
-import stripe, { Stripe } from '@/src/lib/stripe'
 import { event } from 'app'
-import { fuego } from '@nandorojo/swr-firestore'
 import Loading from '@/src/components/loading'
-import { GetStaticPaths, GetStaticProps, GetServerSideProps } from 'next'
-import initFirebaseAdmin from '@/src/lib/initFirebaseAdmin'
 import withAuth from '@/src/lib/withAuth'
 import useSWR from 'swr'
+import { useCollection } from '@nandorojo/swr-firestore'
+import { useRouter } from 'next/router'
+import { NextPage } from 'next'
 
 const fetcher = async (url, user) => {
   const res = await fetch(url, {
@@ -24,14 +23,31 @@ const fetcher = async (url, user) => {
   return individual ? individual.requirements : null
 }
 
-const MyEvents = ({ user, events }) => {
+const MyEvents: NextPage<{user: firebase.User}> = ({user}) => {
+  const router = useRouter()
   const { data: requirements } = useSWR(user && ['/api/stripeAccountsRetrieve', user], fetcher)
+  const {data: eventsData} = useCollection<event>(router && `events`, {
+    listen: true,
+    where: ['createdUser', '==', router?.query?.id],
+  })
+  const [events, setEvents] = useState<({thumbnail: string} & event)[]>([])
+  
+  useEffect(() => {
+    if (!user || !eventsData) return
+    Promise.all(eventsData.map(async eventData => {
+      const thumbnail =
+        eventData.photos.length > 0
+          ? await getImg(eventData.photos[0], user.uid, '360')
+          : await getImg(null, user.uid, '360')
+      return { ...eventData, thumbnail }
+    })).then((result) => setEvents(result as ({thumbnail: string} & event)[]))
+  }, [user, eventsData])
 
   const renderUserEvents = () =>
     events.map((event, i) => {
       const showDate = () => {
-        const startDate = moment(event.startDate)
-        const endDate = moment(event.endDate)
+        const startDate = moment(event.startDate.toDate())
+        const endDate = moment(event.endDate.toDate())
         if (startDate.format('YYYYMD') === endDate.format('YYYYMD')) {
           return `${startDate.format('YYYY年 M月D日  H:mm')} - ${endDate.format('H:mm')}`
         } else {
@@ -48,7 +64,7 @@ const MyEvents = ({ user, events }) => {
               <CardBody>
                 <Row>
                   <Col sm="2" xs="3">
-                    <img width="100%" src={event.photos} alt="image" />
+                    <img width="100%" src={event.thumbnail} alt="image" />
                   </Col>
                   <Col xs="auto">
                     <CardTitle>{event.name}</CardTitle>
@@ -63,7 +79,7 @@ const MyEvents = ({ user, events }) => {
       )
     })
 
-  if (requirements === undefined) return <Loading />
+  if (!events || requirements === undefined) return <Loading />
 
   return (
     <>
@@ -101,29 +117,6 @@ const MyEvents = ({ user, events }) => {
       })()}
     </>
   )
-}
-
-export const getServerSideProps: GetServerSideProps = async ({ query }) => {
-  const { firestore } = await initFirebaseAdmin()
-  const { id } = query
-  const result = await firestore.collection('events').where('createdUser', '==', id).get()
-  const events = await Promise.all(
-    result.docs.map(async (doc) => {
-      const data = doc.data() as event
-      const startDate = data.startDate.toMillis()
-      const endDate = data.endDate.toMillis()
-      const photos =
-        data.photos.length > 0
-          ? await getImg(data.photos[0], id as string, '360')
-          : await getImg(null, id as string, '360')
-      return { ...data, startDate, endDate, photos, id: doc.id }
-    }),
-  )
-
-  return {
-    props: { events },
-    // revalidate: 1
-  }
 }
 
 export default withAuth(MyEvents)
