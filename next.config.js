a89b4ce725e59e7e896e52bd8257a9cf3483f7d1
Hiraki Tomohiko <hirakitomohiko@gmail.com>
2021-02-16 12:16:41 +0900

    モジュールアップデート

--- next.config.js
+++ next.config.js
@@ -16,6 +16,7 @@ module.exports = withPWA(
       dest: 'public',
       disable: process.env.NODE_ENV === 'development',
     },
+    future: { webpack5: true },
     webpack: (config) => {
       config.plugins.push(
         new MomentLocalesPlugin({
